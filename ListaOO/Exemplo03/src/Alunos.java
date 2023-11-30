dc63a432119d77478a917733c61b6ade66743e4e
Brunno Oliveira <92534443+BlackPearlBP@users.noreply.github.com>
2023-11-30 11:26:16 -0300

    Add files via upload

--- ListaOO/Exemplo03/src/Alunos.java
+++ ListaOO/Exemplo03/src/Alunos.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 
 public class Alunos {
-    ArrayList<Alunos> listaDeAlunos=new ArrayList<Alunos>();
     public String nome;
     public int serie;
     public int idade;
+    public int quantidadeNotas;
+    ArrayList<Notas> listaDeNotas=new ArrayList<Notas>();
     
+    public void adicionarNotaAluno(Notas n){
+        this.listaDeNotas.add(n);
+    }
 }
